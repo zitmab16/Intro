c56efcd42a1ed5630a4dcea2a9c5298e87d636a3
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-09-26 12:52:27 +0200

    Implemented Load-Methode

--- Jg3_SerializeIntro/src/SchuelerBL.java
+++ Jg3_SerializeIntro/src/SchuelerBL.java
@@ -1,5 +1,10 @@
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 
@@ -10,10 +15,28 @@ public class SchuelerBL {
     public void add(Schueler s){
         klasse.add(s);
     }
-    public void save(File f){
+    public void save(File f)throws Exception{
+        BufferedWriter bw=new BufferedWriter(new FileWriter(f));
         
+        for (Schueler schueler : klasse) {
+            bw.write(schueler.getName());
+            bw.write(";");
+            bw.write(schueler.getBirthday().toString());
+            bw.newLine();
+        }
     }
-    public void load(File f){
+    public void load(File f)throws Exception{
+        BufferedReader br = new BufferedReader(new FileReader(f));
+
+        String line;
+        while((line=br.readLine()) !=null){
+            String []parts =line.split(";");
+            try{
+            add(new Schueler(parts[0],LocalDate.parse(parts[1])));
+            }catch(Exception ex){
+                System.out.println(ex.getMessage());
+            }
+        }
         
     }
     public static void main(String[] args) {
