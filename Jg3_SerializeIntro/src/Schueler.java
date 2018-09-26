a47876c2b706b6dc0d58fd0af9d2c8526cb5b1fc
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-09-26 12:43:49 +0200

    created classes Schueler and SchuelerBl

---
+++ Jg3_SerializeIntro/src/Schueler.java
@@ -0,0 +1,43 @@
+
+import java.time.LocalDate;
+
+
+public class Schueler {
+    private String name;
+    private int age;
+    private LocalDate birthday;
+
+    public Schueler(String name, LocalDate birthday) {
+        this.name = name;
+        this.birthday = birthday;
+        this.age=LocalDate.now().getYear()-birthday.getYear();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+    
+    
+    
+    
+}
---
+++ Jg3_SerializeIntro/src/SchuelerBL.java
@@ -0,0 +1,19 @@
+
+import java.io.File;
+import java.util.ArrayList;
+
+
+public class SchuelerBL {
+    private ArrayList<Schueler> klasse = new ArrayList<>();
+    
+    
+    public void add(Schueler s){
+        klasse.add(s);
+    }
+    public void save(File f){
+        
+    }
+    public void load(File f){
+        
+    }
+}
